deeb71c9098ca6620ac1c5e2645c2f7ab5f756f2
Alex-At-Home <alex.piggottfamily.co.uk>
2019-10-26 14:10:23 -0400

    Add adjusted efficiency to the team stats table

--- src/components/TeamStatsTable.tsx
+++ src/components/TeamStatsTable.tsx
@@ -31,9 +31,21 @@ const TeamStatsTable: React.FunctionComponent<Props> = ({teamStats}) => {
   const offCellMetaFn = (key: string, val: any) => "off";
   const defPrefixFn = (key: string) => "def_" + key;
   const defCellMetaFn = (key: string, val: any) => "def";
-  const teamStatsOn = { off_title:  "'On' Offense", def_title: "'On' Defense", ...teamStats.on };
-  const teamStatsOff = { off_title:"'Off' Offense", def_title: "'Off' Defense", ...teamStats.off };
-  const teamStatsBaseline = { off_title: "'Baseline' Offense", def_title: "'Baseline' Defense", ...teamStats.baseline };
+  const avgOff2018 = 104.3; //TODO - lookup vs year
+  const calcAdfEff = (stats: any) => {
+    return {
+      off_adj_ppp: stats.def_adj_opp ?
+        (stats.off_ppp.value || 100.0)*(avgOff2018/stats.def_adj_opp.value) : undefined,
+      def_adj_ppp: stats.off_adj_opp ?
+        (stats.def_ppp.value || 100.0)*(avgOff2018/stats.off_adj_opp.value) : undefined
+    };
+  };
+  const adjOn = calcAdfEff(teamStats.on);
+  const adjOff = calcAdfEff(teamStats.off);
+  const adjBase = calcAdfEff(teamStats.baseline);
+  const teamStatsOn = { off_title:  "'On' Offense", def_title: "'On' Defense", ...teamStats.on, ...adjOn };
+  const teamStatsOff = { off_title:"'Off' Offense", def_title: "'Off' Defense", ...teamStats.off, ...adjOff };
+  const teamStatsBaseline = { off_title: "'Baseline' Offense", def_title: "'Baseline' Defense", ...teamStats.baseline, ...adjBase };
   const tableData = [
     GenericTableOps.buildDataRow(teamStatsOn, offPrefixFn, offCellMetaFn),
     GenericTableOps.buildDataRow(teamStatsOn, defPrefixFn, defCellMetaFn),
@@ -50,21 +62,23 @@ const TeamStatsTable: React.FunctionComponent<Props> = ({teamStats}) => {
   const tableFields = { //accessors vs column metadata
     "title": GenericTableOps.addTitle("", ""),
     "sep0": GenericTableOps.addColSeparator(),
-    "ppp": GenericTableOps.addPtsCol("pp100", "Points per 100 possessions", picker(...CbbColors.pp100)),
+    "ppp": GenericTableOps.addPtsCol("P/100", "Points per 100 possessions", picker(...CbbColors.pp100)),
+    "adj_ppp": GenericTableOps.addPtsCol("Adj P/100", "Approximate schedule-adjusted Points per 100 possessions", picker(...CbbColors.pp100)),
+    "sep1": GenericTableOps.addColSeparator(),
     "efg": GenericTableOps.addPctCol("eFG%", "Effective field goal% (3 pointers count 1.5x as much) for selected lineups", picker(...CbbColors.eFG)),
     "to": GenericTableOps.addPctCol("TO%", "Turnover % for selected lineups", picker(...CbbColors.tOver)),
     "orb": GenericTableOps.addPctCol("ORB%", "Offensive rebounding % for selected lineups", picker(...CbbColors.oReb)),
     "ftr": GenericTableOps.addPctCol("FTR", "Free throw rate for selected lineups", picker(...CbbColors.ftr)),
-    "sep1": GenericTableOps.addColSeparator(),
+    "sep2": GenericTableOps.addColSeparator(),
     "3pr": GenericTableOps.addPctCol("3PR", "Percentage of 3 pointers taken against all field goals", picker(...CbbColors.fgr)),
     "2pmidr": GenericTableOps.addPctCol("2PR rim", "Percentage of mid range 2 pointers taken against all field goals", picker(...CbbColors.fgr)),
     "2primr": GenericTableOps.addPctCol("2PR mid", "Percentage of layup/dunk/etc 2 pointers taken against all field goals", picker(...CbbColors.fgr)),
-    "sep2": GenericTableOps.addColSeparator(),
+    "sep3": GenericTableOps.addColSeparator(),
     "3p": GenericTableOps.addPctCol("3P%", "3 point field goal percentage", picker(...CbbColors.fg3P)),
     "2p": GenericTableOps.addPctCol("2P%", "2 point field goal percentage", picker(...CbbColors.fg2P)),
     "2pmid": GenericTableOps.addPctCol("2P% mid", "2 point field goal percentage (mid range)", picker(...CbbColors.fg2P_mid)),
     "2prim": GenericTableOps.addPctCol("2P% rim", "2 point field goal percentage (layup/dunk/etc)", picker(...CbbColors.fg2P_rim)),
-    "sep3": GenericTableOps.addColSeparator(),
+    "sep4": GenericTableOps.addColSeparator(),
     "poss": GenericTableOps.addIntCol("Poss", "Total number of possessions for selected lineups", GenericTableOps.defaultColorPicker),
     "adj_opp": GenericTableOps.addPtsCol("SoS", "Weighted average of the offensive or defensive efficiencies of the lineups' opponents", GenericTableOps.defaultColorPicker),
   };
