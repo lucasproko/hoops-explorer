baceacca5bfa01ab6a82c41538c294752955071f
Alex-At-Home <alex.piggottfamily.co.uk>
2019-10-27 11:23:12 -0400

    Add tooltips (required making the table unresponsive)

--- src/__tests__/setupTests.ts
+++ src/__tests__/setupTests.ts
@@ -4,6 +4,8 @@ import Adapter from 'enzyme-adapter-react-16';
 // Mocking:
 import ClipboardJS from 'clipboard';
 jest.mock("clipboard")
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+jest.mock("react-bootstrap/OverlayTrigger")
 
 
 enzyme.configure({ adapter: new Adapter() });
