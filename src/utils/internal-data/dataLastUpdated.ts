2beea68bf12b490ba1dadb81f84881f59ff4fea2
Alex-At-Home <alex.piggottfamily.co.uk>
2019-11-17 15:36:02 -0500

    Updated efficiency and last update through Sat 16

--- src/utils/internal-data/dataLastUpdated.ts
+++ src/utils/internal-data/dataLastUpdated.ts
@@ -3,6 +3,6 @@
 export const dataLastUpdated: Record<string, number> = { //Update by executing ./get-epoch-time.sh
   "Men_2015/6": 1458864000,
   "Men_2018/9": 1554854400,
-  "Men_2019/20": 1573409134,
+  "Men_2019/20": 1574006356,
   "Women_2018/9": 1554854400
 };
