32050082510ab06c390877c4c6433fcbad867222
Alex-At-Home <alex.piggottfamily.co.uk>
2020-01-12 18:31:20 -0500

    Completed automated testing of new team report on/off algorithm

--- src/utils/LineupUtils.ts
+++ src/utils/LineupUtils.ts
@@ -48,7 +48,7 @@ export class LineupUtils {
 
     // Finish off the weighted averages:
 
-    mutableState.players = _.chain(mutableState.players).flatMap((playerObj) => {
+    mutableState.players = _.chain(mutableState.players).map((playerObj) => {
       if (playerObj.on.hasOwnProperty("off_poss")) {
         LineupUtils.completeWeightedAvg(playerObj.on);
         if (!playerObj.off.hasOwnProperty("off_poss")) {
@@ -57,65 +57,55 @@ export class LineupUtils {
       }
       if (playerObj.off.hasOwnProperty("off_poss")) {
         LineupUtils.completeWeightedAvg(playerObj.off);
-        if (!playerObj.on.hasOwnProperty("off_poss")) {
-          LineupUtils.copyAndZero(playerObj.on, playerObj.off);
-        }
       }
-      //if on/off doesn't contain anything (use off_poss as proxy)
-      // then copy from off but 0 out
-      // + vice-versa .. if neither exist then delete player
-      if (!playerObj.on.hasOwnProperty("off_poss") &&
-        !playerObj.off.hasOwnProperty("off_poss")
-      ) {
-        return [];
-      } else {
-        return [ playerObj ];
-      }
+      return playerObj; // ('ON' exists by construction)
     }).value();
     return mutableState;
   }
 
-  private static readonly ignoreFieldSet = { key: true, players_array: true, doc_count: true };
-  private static readonly sumFieldSet = { off_pos: true, def_pos: true }; //or anything that starts with total_
+  private static readonly ignoreFieldSet =  //or anything that starts with total_
+    { key: true, players_array: true, doc_count: true, points_scored: true, points_allowed: true };
+  private static readonly sumFieldSet = { off_poss: true, def_poss: true };
 
   /** Combines the per-lineup objects into a mutable aggregator */
   private static weightedAvg(mutableAcc: any, obj: any) {
     _.chain(obj).toPairs().forEach((keyVal) => {
       const key = keyVal[0];
-      if (!LineupUtils.ignoreFieldSet.hasOwnProperty(key)) {
-        const val = keyVal[1]?.value;
-        const off_pos = obj.off_pos?.value || 0; //(these are the weights)
-        const def_pos = obj.def_pos?.value || 0;
+      if (!_.startsWith(key, "total_") && !LineupUtils.ignoreFieldSet.hasOwnProperty(key)) {
+        const val = keyVal[1]?.value || 0;
+        const off_poss = obj.off_poss?.value || 0; //(these are the weights)
+        const def_poss = obj.def_poss?.value || 0;
 
-        if (mutableAcc.hasOwnProperty(key)) {
-          if (_.startsWith(key, "total_") || LineupUtils.sumFieldSet.hasOwnProperty(key)) {
-            mutableAcc[key].value += val;
-          } else if (_.startsWith(key, "off_")) { // weighted field, off
-            mutableAcc[key].value += val*off_pos;
-          } else if (_.startsWith(key, "def_")) { // weighted field, off
-            mutableAcc[key].value += val*def_pos;
-          }
-        } else {
-          mutableAcc[key] = { value: val };
+        if (!mutableAcc.hasOwnProperty(key)) {
+          mutableAcc[key] = { value: 0 };
+        }
+        if (LineupUtils.sumFieldSet.hasOwnProperty(key)) {
+          mutableAcc[key].value += val;
+        } else if (_.startsWith(key, "off_")) { // weighted field, off
+          mutableAcc[key].value += val*off_poss;
+        } else if (_.startsWith(key, "def_")) { // weighted field, off
+          mutableAcc[key].value += val*def_poss;
         }
       }
     }).value();
   }
   /** Completes the mutable aggregator by dividing by the sum of its weights*/
   private static completeWeightedAvg(mutableAcc: any) {
-    const off_pos = mutableAcc.off_pos?.value || 1; //(these are the weights)
-    const def_pos = mutableAcc.def_pos?.value || 1;
+    const off_poss = mutableAcc.off_poss?.value || 1; //(these are the weights)
+    const def_poss = mutableAcc.def_poss?.value || 1;
     _.chain(mutableAcc).toPairs().forEach((keyVal) => {
       const key = keyVal[0];
       if (!LineupUtils.ignoreFieldSet.hasOwnProperty(key)) {
         const val = keyVal[1]?.value;
-        if (_.startsWith(key, "off_")) { // weighted field, off
-          mutableAcc[key].value = (1.0*val)/off_pos;
+        if (LineupUtils.sumFieldSet.hasOwnProperty(key)) {
+          //(nothing to do)
+        } else if (_.startsWith(key, "off_")) { // weighted field, off
+          mutableAcc[key].value = (1.0*val)/off_poss;
         } else if (_.startsWith(key, "def_")) { // weighted field, off
-          mutableAcc[key].value = (1.0*val)/def_pos;
+          mutableAcc[key].value = (1.0*val)/def_poss;
         }
       }
-    });
+    }).value();
   }
 
   /** Builds an empty object (all fields 0) */
--- src/utils/__tests__/LineupUtils.test.ts
+++ src/utils/__tests__/LineupUtils.test.ts
@@ -1,4 +1,7 @@
 
+
+import _ from 'lodash';
+
 import { LineupUtils } from "../LineupUtils"
 import { GameFilterParams, LineupFilterParams, TeamReportFilterParams } from "../utils/FilterModels";
 import { sampleLineupStatsResponse } from "../../sample-data/sampleLineupStatsResponse";
@@ -15,21 +18,71 @@ describe("LineupUtils", () => {
 
     const onOffReport = LineupUtils.lineupToTeamReport(lineupReport);
 
-    expect(onOffReport).toEqual({});
-    // HistoryManager.addParamsToHistory("game-year=2019/20&gender=Men");
-    // HistoryManager.addParamsToHistory("lineup-year=2018/9&gender=Women");
-    // expect(HistoryManager.getHistory()).toEqual(
-    //   [
-    //     ["lineup-", { year: "2018/9", gender: "Women" }],
-    //     ["game-", { year: "2019/20", gender: "Men" }],
-    //   ]
-    // )
-    // HistoryManager.addParamsToHistory("game-year=2019/20&gender=Men");
-    // expect(HistoryManager.getHistory()).toEqual(
-    //   [
-    //     ["game-", { year: "2019/20", gender: "Men" }],
-    //     ["lineup-", { year: "2018/9", gender: "Women" }],
-    //   ]
-    // )
+    const playerList = _.chain(onOffReport.players || []).flatMap((onOff) => {
+      return [ onOff.on.key, onOff.off.key ];
+    }).sortBy().value();
+
+    // Check list of expected players
+    expect(playerList).toEqual(
+      [
+        "'OFF' Ayala, Eric", //1,2
+        "'OFF' Cowan, Anthony", //1,2,3
+        "'OFF' Morsell, Darryl", //1,3
+        "'OFF' Scott, Donta", //2,3
+        "'OFF' Smith, Jalen", //1,2
+        "'OFF' Wiggins, Aaron", //1,2,3
+        "'ON' Ayala, Eric",
+        "'ON' Cowan, Anthony",
+        "'ON' Morsell, Darryl",
+        "'ON' Scott, Donta",
+        "'ON' Smith, Jalen",
+        "'ON' Wiggins, Aaron",
+      ]
+    );
+
+    // Check Wiggins "OFF" values are zero'd
+
+    const onOnlyOffVals = _.chain(onOffReport.players).filter((p) => {
+      return p.off.key == "'OFF' Wiggins, Aaron";
+    }).map((p) => p.off).flatMap((off) => {
+      return _.chain(off).toPairs().filter((kv) => {
+        return !_.isObject(kv[1]) || ((kv[1] as any).value != 0)
+      }).value();
+    }).fromPairs().value();
+
+    expect(onOnlyOffVals).toEqual({ key: "'OFF' Wiggins, Aaron" }); //(all vals are 0)
+
+    // Spot check some values
+
+    const toFixed = (obj: any) => {
+      return obj.hasOwnProperty("value") ? { value: obj.value.toFixed(3) } : obj
+    };
+
+    const someOnOffVals = _.chain(onOffReport.players).filter((p) => {
+      return p.on.key == "'ON' Ayala, Eric";
+    }).flatMap((p) => [ p.on, p.off ]).map((onOrOff) => {
+      return _.chain(onOrOff).pick([
+        "key", "off_poss", "def_poss", "off_ppp", "def_ppp",
+        "total_off_fga", "points_scored", "doc_count", "player_array" // (these are all ignored)
+      ]).mapValues(toFixed).value();
+    }).value();
+
+    const totalOffPoss = 192 + 102;
+    const totalDefPoss = 185 + 97;
+    expect(someOnOffVals).toEqual([
+      _.mapValues({
+        key: "'ON' Ayala, Eric",
+        off_poss: { value: totalOffPoss }, def_poss: { value: totalDefPoss },
+        off_ppp: { value: 192.0/(totalOffPoss)*112.5 + 102.0/(totalOffPoss)*109.80392156862744 },
+        def_ppp: { value: 185.0/(totalDefPoss)*89.1891891891892 + 97.0/(totalDefPoss)*80.41237113402062 },
+      }, toFixed),
+      _.mapValues({
+        key: "'OFF' Ayala, Eric",
+        off_poss: { value: 101 }, def_poss: { value: 103 },
+        off_ppp: { value: 91.08910891089108 },
+        def_ppp: { value: 79.6116504854369 },
+      }, toFixed),
+    ]);
+
   });
 });
