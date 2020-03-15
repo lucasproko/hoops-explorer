cab5a8dad9877e0074efe4847c8fd289da0e7f00
Alex-At-Home <alex.piggottfamily.co.uk>
2020-03-15 13:10:42 -0400

    Add older efficiency information, finish up 19/20 season

--- src/utils/internal-data/dataLastUpdated.ts
+++ src/utils/internal-data/dataLastUpdated.ts
@@ -1,9 +1,14 @@
 
 /** The date when the data for the given year was last updated - will clear local caching */
 export const dataLastUpdated: Record<string, number> = { //Update by executing ./get-epoch-time.sh
+  "Men_2014/5": 1428624000,
   "Men_2015/6": 1458864002,
+  "Men_2016/7": 1491782400,
+  "Men_2017/8": 1523318400,
   "Men_2018/9": 1554854403,
   "Women_2018/9": 1554854406,
-  "Men_2019/20": parseInt(process.env.MEN_CURR_UPDATE || "0"),
-  "Women_2019/20": parseInt(process.env.WOMEN_CURR_UPDATE || "0")
+  "Men_2019/20": 1586476800,
+  "Women_2019/20": 1586476800
+  // "Men_2020/21": parseInt(process.env.MEN_CURR_UPDATE || "0"),
+  // "Women_2020/21": parseInt(process.env.WOMEN_CURR_UPDATE || "0")
 };
--- src/utils/public-data/efficiencyAverages.ts
+++ src/utils/public-data/efficiencyAverages.ts
@@ -1,7 +1,10 @@
 export const efficiencyAverages: Record<string, number> = {
+  "Men_2014/5": 104.2,
   "Men_2015/6": 104.8,
+  "Men_2016/7": 104.7,
+  "Men_2017/8": 105.0,
   "Men_2018/9": 104.3,
-  "Men_2019/20": 102.1,
+  "Men_2019/20": 102.4,
   "Women_2018/9": 93.4,
   "Women_2019/20": 91.93
 };
