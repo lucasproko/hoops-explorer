dfd0a8ded6817595d9f138113b1562f6ddaa8c60
Alex-At-Home <alex.piggottfamily.co.uk>
2020-03-28 16:35:58 -0400

    Complete ORtg diagnostic mode

--- src/utils/StatsUtils.ts
+++ src/utils/StatsUtils.ts
@@ -1,9 +1,10 @@
 
 /** All the info needed to explain the ORtg calculation, see "buildORtgDiag" */
 export type ORtgDiagnostics = {
-  // 1] Points produced calcs:
-
   // Basic player numbers:
+  rawFga: number,
+  rawFgx: number,
+  rawFgm: number,
   ptsFgm: number,
   ptsFtm: number,
   rawFtm: number,
@@ -11,16 +12,25 @@ export type ORtgDiagnostics = {
   rawPts: number,
   rawOrb: number,
   rawFgPts: number,
-  // Advanced player numbers:
-  eFG: number,
+  rawTo: number,
   // Basic team numbers:
   teamOrb: number,
   teamPts: number,
+  teamFga: number,
   teamFgm: number,
+  teamFta: number,
+  teamFtPct: number,
   teamOrbPct: number,
+  teamTo: number,
+
+  // 1] Points produced calcs:
+
+  // Advanced player numbers:
+  eFG: number,
   // Advanced team numbers:
   teamPtsPerScore: number,
   teamFtHitOnePlus: number,
+  teamProbFtHitOnePlus: number,
   teamOrbCreditToRebounder: number,
   teamOrbCreditToScorer: number,
   teamScoreFromReboundPct: number,
@@ -41,10 +51,27 @@ export type ORtgDiagnostics = {
   ppFg: number,
 
   // 2] Possession Calcs:
-  adjPoss: number,
+  // Advanced player numbers:
+  offPoss: number,
+  ftPoss: number,
+  ftPct: number,
+  missedBothFTs: number,
+  offPlaysLessPoss: number,
+  fgPart: number,
+  ftPart: number,
+  astPart: number,
+  orbPart: number,
+  // Advanced team numbers:
   teamScoringPoss: number,
   teamPlays: number,
+  // Possession calcs
+  adjPoss: number,
+  scoringPoss: number,
+  fgxPoss: number,
+  ftxPoss: number
 }
+
+
 
 /** General cbb complex stats calcs */
 export class StatsUtils {
@@ -65,6 +92,7 @@ export class StatsUtils {
     const ORB = statSet?.total_off_orb?.value || 0;
     const FG2PM = statSet?.total_off_2p_made?.value || 0;
     const FG3PM = statSet?.total_off_3p_made?.value || 0;
+    const offPoss = statSet?.off_poss?.value || 0;
 
     const Team_AST = statSet?.team_total_off_assist?.value || 0;
     const Team_FGM = statSet?.team_total_off_fgm?.value || 0;
@@ -94,9 +122,11 @@ export class StatsUtils {
     const AST_Part = Others_FGA > 0 ?
       0.5 * ((Team_PTS_FROM_FG - PTS_FROM_FG) / (2 * Others_FGA)) * AST : 0.0;
 
-    const FT_Part = FTA > 0 ? (1-(1-(FTM/FTA))**2)*FTA_to_Poss*FTA : 0.0;
+    const Prob_Miss_Both_FT = (1-(FTM/FTA))**2
+    const FT_Part = FTA > 0 ? (1-Prob_Miss_Both_FT)*FTA_to_Poss*FTA : 0.0;
 
-    const Team_Scoring_Poss = Team_FTA > 0 ? Team_FGM + (1 - (1 - (Team_FTM / Team_FTA))^2) * Team_FTA * FTA_to_Poss : 0.0;
+    const Team_Prob_Hit_1plus_FT = (1 - (1 - (Team_FTM / Team_FTA))**2);
+    const Team_Scoring_Poss = Team_FTA > 0 ? Team_FGM + Team_Prob_Hit_1plus_FT * Team_FTA * FTA_to_Poss : 0.0;
 
     const Team_ORB_pct = (Team_ORB + Opponent_DRB) > 0 ? Team_ORB/(Team_ORB + Opponent_DRB) : 0.0;
     const Num_Team_Plays = Team_FGA + Team_FTA * FTA_to_Poss + Team_TOV;
@@ -106,18 +136,21 @@ export class StatsUtils {
     const Credit_To_Scorer = Team_ORB_pct * (1 - Team_Play_Pct);
     const Team_ORB_Weight_Denom = Credit_To_Rebounder + Credit_To_Scorer;
     const Team_ORB_Weight = Team_ORB_Weight_Denom > 0 ?  Credit_To_Rebounder/ Team_ORB_Weight_Denom : 0.0;
+    const Team_Score_Rebound_Pct =
+      Team_Scoring_Poss > 0 ? (Team_ORB * Team_Play_Pct) / Team_Scoring_Poss : 0.0;
+    const Team_ORB_Contrib = Team_ORB_Weight * Team_Score_Rebound_Pct;
 
     const ORB_Part = ORB * Team_ORB_Weight * Team_Play_Pct;
 
     // And then:
     const ScPoss = Team_Scoring_Poss > 0 ? (FG_Part + AST_Part + FT_Part) *
-      (1 - (Team_ORB / Team_Scoring_Poss) * Team_ORB_Weight * Team_Play_Pct) + ORB_Part : 0;
+      (1 - Team_ORB_Contrib) + ORB_Part : 0;
 
     // Other factors:
 
     const FGxPoss = (FGA - FGM) * (1 - 1.07 * Team_ORB_pct);
 
-    const FTxPoss = FTA > 0 ? ((1 - (FTM / FTA))**2) * FTA_to_Poss * FTA : 0.0;
+    const FTxPoss = FTA > 0 ? Prob_Miss_Both_FT * FTA_to_Poss * FTA : 0.0;
 
     const TotPoss = ScPoss + FGxPoss + FTxPoss + TOV;
 
@@ -131,36 +164,40 @@ export class StatsUtils {
     const Other_Pts_Per_FGM = Others_FGM > 0 ? (Team_PTS_FROM_FG - PTS_FROM_FG) / Others_FGM : 0.0;
     const PProd_AST_Part = (0.5*Other_eFG) * AST * Other_Pts_Per_FGM;
 
-    const Prob_Hit_1plus_FT = (1 - (1 - (Team_FTM / Team_FTA))**2);
-    const Team_FTs_Hit_1plus = Team_FTA > 0 ? Prob_Hit_1plus_FT * FTA_to_Poss * Team_FTA : 0.0;
+    const Team_FTs_Hit_1plus = Team_FTA > 0 ? Team_Prob_Hit_1plus_FT * FTA_to_Poss * Team_FTA : 0.0;
     const Team_Pts_Per_Score = (Team_FGM + Team_FTs_Hit_1plus) > 0 ? Team_PTS / (Team_FGM + Team_FTs_Hit_1plus) : 0.0;
     const PProd_ORB_Part = ORB * Team_ORB_Weight * Team_Play_Pct * Team_Pts_Per_Score;
 
-    const Team_Score_Rebound_Pct =
-      Team_Scoring_Poss > 0 ? (Team_ORB * Team_Play_Pct) / Team_Scoring_Poss : 0.0;
-    const Team_ORB_Contrib = Team_ORB_Weight * Team_Score_Rebound_Pct;
-
     const PProd = (PProd_FG_Part + PProd_AST_Part + FTM) * (1 - Team_ORB_Contrib) + PProd_ORB_Part;
 
-    return [{ value: 100 * (PProd / TotPoss) }, (calcDiags ? {
-      // 1] Points produced calcs:
-
+    return [TotPoss > 0 ? { value: 100 * (PProd / TotPoss) } : undefined, (calcDiags ? {
       // Basic player numbers:
+      rawFga: FGA,
+      rawFgx: FGA - FGM,
+      rawFgm: FGM,
       ptsFgm: PTS_FROM_FG,
       rawFtm: FTM,
       rawAssist: AST,
       rawPts: PTS_FROM_FG + FTM,
       rawOrb: ORB,
-      // Advanced player numbers:
-      eFG: eFG,
+      rawTo: TOV,
       // Basic team numbers:
       teamOrb: Team_ORB,
       teamPts: Team_PTS,
+      teamFga: Team_FGA,
       teamFgm: Team_FGM,
+      teamFta: Team_FTA,
+      teamFtPct: Team_FTA > 0 ? Team_FTM / Team_FTA : 0,
       teamOrbPct: Team_ORB_pct,
+      teamTo: Team_TOV,
+
+      // 1] Points produced calcs:
+      // Advanced player numbers:
+      eFG: eFG,
       // Advanced team numbers:
       teamPtsPerScore: Team_Pts_Per_Score,
       teamFtHitOnePlus: Team_FTs_Hit_1plus,
+      teamProbFtHitOnePlus: Team_Prob_Hit_1plus_FT,
       teamOrbCreditToRebounder: Credit_To_Rebounder,
       teamOrbCreditToScorer: Credit_To_Scorer,
       teamScoreFromReboundPct: Team_Score_Rebound_Pct,
@@ -180,9 +217,24 @@ export class StatsUtils {
       ppFg: PProd_FG_Part,
 
       // 2] Possession Calcs:
-      adjPoss: TotPoss,
+      // Advanced player numbers:
+      ftPoss: FTA_to_Poss*FTA,
+      ftPct: FTA > 0 ? FTM / FTA : 0,
+      missedBothFTs: Prob_Miss_Both_FT,
+      offPlaysLessPoss: FGA + FTA*FTA_to_Poss + TOV - offPoss,
+      offPoss: offPoss,
+      fgPart: FG_Part,
+      ftPart: FT_Part,
+      astPart: AST_Part,
+      orbPart: ORB_Part,
+      // Advanced team numbers:
       teamScoringPoss: Team_Scoring_Poss,
       teamPlays: Num_Team_Plays,
+      // Possessions calcs
+      adjPoss: TotPoss,
+      scoringPoss: ScPoss,
+      fgxPoss: FGxPoss,
+      ftxPoss: FTxPoss
 
     } : undefined) as (ORtgDiagnostics | undefined) ];
   }
