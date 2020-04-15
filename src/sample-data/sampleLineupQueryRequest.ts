577fbc83b3d97d831179dd9bfb96e438c165b5fc
Alex-At-Home <alex.piggottfamily.co.uk>
2020-04-14 21:36:35 -0400

    Update unit tests for all the different query combos

--- src/sample-data/sampleLineupQueryRequest.ts
+++ src/sample-data/sampleLineupQueryRequest.ts
@@ -582,56 +582,56 @@ export const sampleLineupQueryRequest =         {
             }
          },
          "query": {
-            "bool": {
-               "filter": [],
-               "must_not": [],
-               "should": [],
-               "must": [
-                  {
-                     "bool": {
-                        "filter": [],
-                        "must_not": [],
-                        "should": [],
-                        "minimum_should_match": 0,
-                        "must": [
-                           {
+           "bool": {
+              "filter": [],
+              "must_not": [],
+              "should": [],
+              "must": [
+                {
+                  "bool": {
+                     "filter": [],
+                     "must_not": [],
+                     "should": [],
+                     "minimum_should_match": 0,
+                     "must": [
+                        {
+                           "term": {
+                              "team.team.keyword": "TestTeam"
+                           }
+                        },
+                        {
+                           "script": {
                               "script": {
-                                 "script": {
-                                    "source": "if (params.kp.isEmpty()) return true;\ndef kp_name = params.pbp_to_kp[doc[\"opponent.team.keyword\"].value];\nif (kp_name == null) {\n   kp_name = doc[\"opponent.team.keyword\"].value;\n} else {\n   kp_name = kp_name.pbp_kp_team\n}\ndef oppo = params.kp[kp_name];\nif (oppo != null) {\n def kp_rank = oppo['stats.adj_margin.rank'];\n def game_filter = params.game_filter.game_filter;\n //TODO: high major\n return (kp_rank >= game_filter.min_kp) && (kp_rank <= game_filter.max_kp);\n} else {\n return false;\n}\n\n",
-                                    "lang": "painless",
-                                    "params": {
-                                       "pbp_to_kp": {
-                                          "name1": "name1b"
-                                       },
-                                       "kp": {
-                                          "team": {
-                                             "stats": 0
-                                          }
-                                       },
-                                       "game_filter": {
-                                          "game_filter": {
-                                             "min_kp": 10,
-                                             "max_kp": 100
-                                          }
+                                 "source": "\n                  if (params.kp.isEmpty()) return true;\n                  def kp_name = params.pbp_to_kp[doc[\"opponent.team.keyword\"].value];\n                  if (kp_name == null) {\n                     kp_name = doc[\"opponent.team.keyword\"].value;\n                  } else {\n                     kp_name = kp_name.pbp_kp_team;\n                  }\n                  def oppo = params.kp[kp_name];\n                  if (oppo != null) {\n                     def kp_rank = oppo[\"stats.adj_margin.rank\"];\n                     def game_filter = params.game_filter;\n                     def oppo_conf = oppo[\"conf\"];\n                     def conf_allowed = true;\n                     if (!game_filter.conf.isEmpty()) {\n                        conf_allowed = game_filter.conf.equals(oppo_conf);\n                     }\n                     return conf_allowed && (kp_rank >= game_filter.min_kp) && (kp_rank <= game_filter.max_kp);\n                  } else {\n                      return false;\n                  }\n                 ",
+                                 "lang": "painless",
+                                 "params": {
+                                    "pbp_to_kp": {
+                                       "name1": "name1b"
+                                    },
+                                    "kp": {
+                                       "team": {
+                                          "stats": 0
                                        }
+                                    },
+                                    "game_filter": {
+                                       "min_kp": 10,
+                                       "max_kp": 100,
+                                       "conf": ""
                                     }
                                  }
                               }
-                           },
-                           {
-                              "term": {
-                                 "team.team.keyword": "undefined"
-                              }
                            }
-                        ]
-                     }
-                  },
-                  {
-                     "query_string": {
-                        "query": "players.id:(base)"
-                     }
+                        }
+                     ]
                   }
-               ]
+
+                },
+                {
+                   "query_string": {
+                      "query": "players.id:(base)"
+                   }
+                }
+              ]
             }
          }
       };
