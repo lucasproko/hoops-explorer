1e69cbd6d2aa4117d18c9e153bd728b3eaf36f89
Alex-At-Home <alex.piggottfamily.co.uk>
2020-06-02 18:25:20 -0400

    Add loading overlay and fix formatting of axis expalantion for positional chart

--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -26,8 +26,9 @@ import Alert from 'react-bootstrap/Alert';
 import Footer from '../components/Footer';
 import HeaderBar from '../components/HeaderBar';
 
+// Additional components:
 // @ts-ignore
-import Iframe from 'react-iframe'
+import LoadingOverlay from 'react-loading-overlay';
 
 // Utils:
 
@@ -83,6 +84,17 @@ const ChartsPage: NextPage<{}> = () => {
     }
   }
 
+  // Positional chart loading?
+  const [ positionChartLoading, setPositionChartLoading ] = useState(true);
+  const highMajorPositionChart: React.RefObject<HTMLIFrameElement> = React.createRef()
+
+  useEffect(() => {
+    // On window load, if iframe has insta-loaded then onLoad doesn't fire so clear here:
+    if (highMajorPositionChart?.current?.contentDocument?.readyState == 'complete') {
+      setPositionChartLoading(false);
+    }
+  }, []);
+
   return <Container>
     <Row>
       <Col xs={12} className="text-center">
@@ -110,30 +122,43 @@ const ChartsPage: NextPage<{}> = () => {
             <Tab.Content>
               <Tab.Pane eventKey="high_major_positions">
                 <div>
-                  <Alert variant="info" className="small">
-                    <em>
-                      A 2-d visualization of the different positions (P6 2010-2020, all D1 2015-2020).
-                      {maybeShowBlogArticle(tabKey)}
-                      &nbsp;The data came from <a target="_blank" href="http://adamcwisports.blogspot.com/p/data.html">barttorvik.com</a>, much appreciated!<br/>
-                      The graph below is interactive - you can scroll in or out, move around, highlight specific teams, etc. Note - it can be a bit slow!<br/>
-                    </em>
-                  </Alert>
-                  <iframe frameBorder="0" src="/high_major_positions.html" width="1024px" height="700px"/>
-                  <Alert variant="info" className="small">
-                    <em>
-                      The x- and y- axis are linear combination of the input features that are hard to interpret in basketball terms. But basically:
-                      <li>Left-to-right is "guard-like" to "forward-like":</li>
-                      <ul>
-                        <li>leftwards: 3P%, Assist rate, 3P rate, steal rate</li>
-                        <li>rightwards: DRB%, ORB%, dunk rate, mid-2P rate, block rate, foul rate</li>
-                      </ul>
-                      <li>Top-to-bottom is a mishmash of position-specific bonuses/penalties: combined because we only have 2-d to play with!</li>
-                      <ul>
-                        <li>upwards: AST/TOV, Assist rate, TO rate, steal rate; dunk/rim rate, mid-2P rate, block rate</li>
-                        <li>downwards: 3P rate, rim shooting ability, assists/FG; DRB%, ORB%</li>
-                      </ul>
-                    </em>
-                  </Alert>
+                  <LoadingOverlay
+                    active={positionChartLoading}
+                    text="Loading Chart"
+                  >
+                    <Alert variant="info" className="small">
+                      <em>
+                        A 2-d visualization of the different positions (P6 2010-2020, all D1 2015-2020).
+                        {maybeShowBlogArticle(tabKey)}
+                        &nbsp;The data came from <a target="_blank" href="http://adamcwisports.blogspot.com/p/data.html">barttorvik.com</a>, much appreciated!<br/>
+                        The graph below is interactive - you can scroll in or out, move around, highlight specific teams, etc. Note - it can be a bit slow!<br/>
+                      </em>
+                    </Alert>
+                    <iframe
+                      onLoad={() => { //(sometimes this fires too early, handled in useEffect above)
+                        setPositionChartLoading(false);
+                      }}
+                      ref={highMajorPositionChart}
+                      frameBorder="0" src="/high_major_positions.html" width="1024px" height="700px"
+                    />
+                    <Alert variant="info" className="small">
+                      <em>
+                        The x- and y- axis are linear combination of the input features that are hard to interpret in basketball terms. But basically:
+                        <ul>
+                          <li>Left-to-right is "guard-like" to "forward-like":</li>
+                          <ul>
+                            <li>leftwards: 3P%, Assist rate, 3P rate, steal rate</li>
+                            <li>rightwards: DRB%, ORB%, dunk rate, mid-2P rate, block rate, foul rate</li>
+                          </ul>
+                          <li>Top-to-bottom is a mishmash of position-specific bonuses/penalties: combined because we only have 2-d to play with!</li>
+                          <ul>
+                            <li>upwards: AST/TOV, Assist rate, TO rate, steal rate; dunk/rim rate, mid-2P rate, block rate</li>
+                            <li>downwards: 3P rate, rim shooting ability, assists/FG; DRB%, ORB%</li>
+                          </ul>
+                        </ul>
+                      </em>
+                    </Alert>
+                  </LoadingOverlay>
                 </div>
               </Tab.Pane>
             </Tab.Content>
