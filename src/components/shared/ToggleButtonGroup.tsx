4d0c32567e99fbaed9ecbd9d0dcf10255d07c5c7
Alex-At-Home <alex.piggottfamily.co.uk>
2020-08-15 09:21:06 -0400

    Fix issue with SSR caching

--- src/components/shared/ToggleButtonGroup.tsx
+++ src/components/shared/ToggleButtonGroup.tsx
@@ -18,23 +18,25 @@ export type ToggleButtonItem = {
 }
 
 type Props = {
-  items: ToggleButtonItem[]
+  items: ToggleButtonItem[],
+  override?: boolean //(for testing, set to true to force it to render)
 }
 
-const ToggleButtonGroup: React.FunctionComponent<Props> = ({items}) => {
+const ToggleButtonGroup: React.FunctionComponent<Props> = ({items, override}) => {
 
   const tooltip = (tooltip: string, i: number) => <Tooltip id={`tooltip-${i}`}>{tooltip}</Tooltip>;
 
-  return <div><small>Quick Select:</small>&nbsp;&nbsp;
-  {
-    items.map((item, index) => {
-      return <span key={"divtog" + index}>
-        <OverlayTrigger placement="auto" overlay={tooltip(item.tooltip, index)}>
-          <Button onClick={item.onClick} size="sm" key={"tog" + index} variant={item.toggled ? "dark" : "outline-secondary"}>{item.label}</Button>
-        </OverlayTrigger>
-        &nbsp;&nbsp;
-        </span>;
-    })
-  }</div>;
+  return (override || (typeof window !== `undefined`)) ?
+    <div><small>Quick Select:</small>&nbsp;&nbsp;
+    {
+      items.map((item, index) => {
+          return <span key={"divtog" + index}>
+            <OverlayTrigger placement="auto" overlay={tooltip(item.tooltip, index)}>
+              <Button onClick={item.onClick} size="sm" key={"tog" + index} variant={item.toggled ? "dark" : "outline-secondary"}>{item.label}</Button>
+            </OverlayTrigger>
+            &nbsp;&nbsp;
+          </span>
+      })
+    }</div> : <div/>; //(this construct needed to address SSR caching issue)
 };
 export default ToggleButtonGroup;
--- src/components/shared/__tests__/ToggleButtonGroup.test.tsx
+++ src/components/shared/__tests__/ToggleButtonGroup.test.tsx
@@ -8,7 +8,27 @@ import renderer from 'react-test-renderer';
 describe("ToggleButtonGroup", () => {
   test("ToggleButtonGroup", () => {
     const component = renderer.create(
-      <ToggleButtonGroup items={[
+      <ToggleButtonGroup override={true} items={[
+        {
+          label: "Test A",
+          tooltip: "Test Tooltip A",
+          toggled: true,
+          onClick: () => {}
+        },
+        {
+          label: "Test B",
+          tooltip: "Test Tooltip A",
+          toggled: false,
+          onClick: () => {}
+        }
+      ]}/>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("ToggleButtonGroup - empty SSR render", () => {
+    const component = renderer.create(
+      <ToggleButtonGroup override={false} items={[
         {
           label: "Test A",
           tooltip: "Test Tooltip A",
