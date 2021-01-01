fdd71b62bc4fb716f819735174d7fc316b292129
Alex-At-Home <alex.piggottfamily.co.uk>
2021-01-01 17:05:30 -0500

    First attempt as showing +- visually, not sure I like it though

--- src/components/diags/GameInfoDiagView.tsx
+++ src/components/diags/GameInfoDiagView.tsx
@@ -8,14 +8,14 @@ import _ from "lodash";
 
 // Bootstrap imports:
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // import Tooltip from 'react-bootstrap/Tooltip';
 // import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 //@ts-ignore
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ReferenceLine, Label } from 'recharts';
+import { ResponsiveContainer, ComposedChart, Bar, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ReferenceLine, Label } from 'recharts';
 
 // Utils
 
@@ -39,6 +39,7 @@ const CustomTooltip: React.FunctionComponent<CustomTooltipProps> = ({ active, pa
           <span>[<b>{data.num_def_poss}</b>] def possessions</span><br/>
           <span>[<b>{data.num_pts_for}</b>] pts scored</span><br/>
           <span>[<b>{data.num_pts_against}</b>] pts conceded</span><br/>
+          <span>+- [<b>{data.plusMinus}</b>]</span><br/>
         </p>
       </div>
     );
@@ -58,43 +59,72 @@ const GameInfoDiagView: React.FunctionComponent<Props> = ({oppoList, orderedOppo
 
   // Merge with the ordered oppo list
   oppoList.forEach((oppo) => {
-    orderedOppoList[`${oppo.date} ${oppo.opponent}`] = oppo;
+    const plusMinus = (oppo.num_pts_for || 0) - (oppo.num_pts_against || 0);
+    const marginalEff = (oppo.num_pts_for || 0)/(oppo.num_off_poss || 1) - (oppo.num_pts_against || 0)/(oppo.num_def_poss || 1);
+    const maybeMin = (t: number) => zoomIn ? t : Math.min(t, 100);
+    orderedOppoList[`${oppo.date} ${oppo.opponent}`] = {
+      ...oppo,
+      plusMinus: plusMinus,
+      plusOnly: marginalEff >= 0 ? maybeMin(100*marginalEff) : undefined,
+      minusOnly: marginalEff < 0 ? maybeMin(100*Math.abs(marginalEff)) : undefined,
+    };
   });
   const gameDates = _.values(orderedOppoList);
 
-
-  return <div>
-    <br/>
-    <ResponsiveContainer width={"80%"} height={120}>
-      <LineChart
-        data={gameDates}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="opponent" />
-        <YAxis domain={[0, zoomIn ? 'auto' : (maxOffPoss + 1)]}>
-          <Label angle={-90} value='# possessions' position='insideLeft' style={{textAnchor: 'middle'}} />
-        </YAxis>
-        <Tooltip
-          content={(<CustomTooltip />)}
-          wrapperStyle={{ opacity: "0.9", zIndex: "1000" }}
-          formatter={(value: number, name: string, props: any) => value.toFixed(0)}
-          allowEscapeViewBox={{x: true, y: true}}
-          itemSorter={(item: any) => <span>item.value</span>}
-        />
-        <Line
-          type="monotone"
-          isAnimationActive={false}
-          dataKey="num_off_poss"
-          stroke="#8884d8"/>
-      </LineChart>
-    </ResponsiveContainer>
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <b>Game breakdown for above lineup</b>
-      &nbsp;(<a href="#" onClick={(event) => { event.preventDefault(); setZoomIn(!zoomIn) }}>{zoomIn ? "zoom out" : "zoom in"}</a>)
-    </div>
-  </div>;
+  return <Container>
+    <Row>
+      <Col xs={1}/>
+      <Col xs={10}>
+        <br/>
+        <ResponsiveContainer width={"100%"} height={120}>
+          <ComposedChart
+            data={gameDates}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="opponent" />
+            <YAxis yAxisId="left" domain={[0, zoomIn ? 'auto' : (maxOffPoss + 1)]}>
+              <Label angle={-90} value='# possessions' position='insideLeft' style={{textAnchor: 'middle'}} />
+            </YAxis>
+            <YAxis yAxisId="right" domain={[0, zoomIn ? 'auto' : 100]} orientation="right" tickFormatter={t => (!zoomIn && (t == 100)) ? "100+" : t.toFixed(0)}>
+              <Label angle={-90} value='margin/100' position='insideRight' style={{textAnchor: 'middle'}} />
+            </YAxis>
+            <Tooltip
+              content={(<CustomTooltip />)}
+              wrapperStyle={{ opacity: "0.9", zIndex: "1000" }}
+              formatter={(value: number, name: string, props: any) => value.toFixed(0)}
+              allowEscapeViewBox={{x: true, y: true}}
+              itemSorter={(item: any) => <span>item.value</span>}
+            />
+            <Bar
+              yAxisId="right"
+              barSize={5}
+              isAnimationActive={false}
+              dataKey="plusOnly"
+              fill="green" fillOpacity={0.5}/>
+            <Bar
+              yAxisId="right"
+              barSize={5}
+              isAnimationActive={false}
+              dataKey="minusOnly"
+              fill="red" fillOpacity={0.5}/>
+            <Line
+              yAxisId="left"
+              type="monotone"
+              isAnimationActive={false}
+              dataKey="num_off_poss"
+              stroke="#8884d8" strokeWidth={2}/>
+          </ComposedChart>
+        </ResponsiveContainer>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <b>Game breakdown for above lineup</b>
+          &nbsp;(<a href="#" onClick={(event) => { event.preventDefault(); setZoomIn(!zoomIn) }}>{zoomIn ? "zoom out" : "zoom in"}</a>)
+        </div>
+      </Col>
+      <Col/>
+    </Row>
+  </Container>;
 };
 export default GameInfoDiagView;
