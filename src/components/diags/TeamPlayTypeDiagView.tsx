a494713a57abe97603f3eb94b5e64fc454e4db02
Alex-At-Home <alex.piggottfamily.co.uk>
2021-02-06 20:02:09 -0500

    Outline code for converting player play type stats into team play type stats

--- src/components/diags/TeamPlayTypeDiagView.tsx
+++ src/components/diags/TeamPlayTypeDiagView.tsx
@@ -15,91 +15,95 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 // Utils
+import { PosFamilyNames, PlayTypeUtils } from "../../utils/stats/PlayTypeUtils";
 import { PositionUtils } from "../../utils/stats/PositionUtils";
 import { CommonTableDefs } from "../../utils/tables/CommonTableDefs";
 import { CbbColors } from "../../utils/CbbColors";
+import { LineupUtils } from "../../utils/stats/LineupUtils";
 
 // Component imports
 import GenericTable, { GenericTableOps, GenericTableColProps } from "../GenericTable";
 
-// Table defs
-/*
-const simpleDiagTable = {
-  "title": GenericTableOps.addTitle("", "The positional class associated with this player's statistical signature"),
-  "sep0": GenericTableOps.addColSeparator(),
-  "pos_pg": GenericTableOps.addPctCol("PG%", tooltipTradPosGenerator("Point Guard"), GenericTableOps.defaultColorPicker),
-  "pos_sg": GenericTableOps.addPctCol("SG%", tooltipTradPosGenerator("Shooting Guard"), GenericTableOps.defaultColorPicker),
-  "pos_sf": GenericTableOps.addPctCol("SF%", tooltipTradPosGenerator("Small Forward"), GenericTableOps.defaultColorPicker),
-  "pos_pf": GenericTableOps.addPctCol("PF%", tooltipTradPosGenerator("Power Forward"), GenericTableOps.defaultColorPicker),
-  "pos_c": GenericTableOps.addPctCol("C%", tooltipTradPosGenerator("Center"), GenericTableOps.defaultColorPicker),
-};
-
-const complexDiagTable = {
-
-  "feature": GenericTableOps.addTitle("", "Each of the statistical features used in the classification", CommonTableDefs.singleLineRowSpanCalculator, "", GenericTableOps.htmlFormatter),
-  "sep0": GenericTableOps.addColSeparator(),
-  "player": GenericTableOps.addPctCol("Player %", "The value of this player vs this statistic", colorPicker),
-  "sep1": GenericTableOps.addColSeparator(),
-  "pos_pg": GenericTableOps.addPctCol("Wt PG", tooltipWeightGenerator("Point Guard"), GenericTableOps.defaultColorPicker),
-  "av_pos_pg": GenericTableOps.addPctCol("Av PG", tooltipAverageGenerator("Point Guard"), colorPicker),
-  "contrib_pos_pg": GenericTableOps.addPctCol("+- PG%", tooltipContribGenerator("Point Guard"), CbbColors.varPicker(CbbColors.posColors)),
-  "sep2": GenericTableOps.addColSeparator(),
-  "pos_sg": GenericTableOps.addPctCol("Wt SG", tooltipWeightGenerator("Shooting Guard"), GenericTableOps.defaultColorPicker),
-  "av_pos_sg": GenericTableOps.addPctCol("Av SG", tooltipAverageGenerator("Shooting Guard"), colorPicker),
-  "contrib_pos_sg": GenericTableOps.addPctCol("+- SG%", tooltipContribGenerator("Shooting Guard"), CbbColors.varPicker(CbbColors.posColors)),
-  "sep3": GenericTableOps.addColSeparator(),
-  "pos_sf": GenericTableOps.addPctCol("Wt SF", tooltipWeightGenerator("Small Forward"), GenericTableOps.defaultColorPicker),
-  "av_pos_sf": GenericTableOps.addPctCol("Av SF", tooltipAverageGenerator("Small Forward"), colorPicker),
-  "contrib_pos_sf": GenericTableOps.addPctCol("+- SF%", tooltipContribGenerator("Small Forward"), CbbColors.varPicker(CbbColors.posColors)),
-  "sep4": GenericTableOps.addColSeparator(),
-  "pos_pf": GenericTableOps.addPctCol("Wt PF", tooltipWeightGenerator("Power Forward"), GenericTableOps.defaultColorPicker),
-  "av_pos_pf": GenericTableOps.addPctCol("Av PF", tooltipAverageGenerator("Power Forward"), colorPicker),
-  "contrib_pos_pf": GenericTableOps.addPctCol("+- PF%", tooltipContribGenerator("Power Forward"), CbbColors.varPicker(CbbColors.posColors)),
-  "sep5": GenericTableOps.addColSeparator(),
-  "pos_c": GenericTableOps.addPctCol("Wt C", tooltipWeightGenerator("Center"), GenericTableOps.defaultColorPicker),
-  "av_pos_c": GenericTableOps.addPctCol("Av C", tooltipAverageGenerator("Center"), colorPicker),
-  "contrib_pos_c": GenericTableOps.addPctCol("+- C%", tooltipContribGenerator("Center"), CbbColors.varPicker(CbbColors.posColors)),
-};
-*/
+const tidyNumbers = (k: string, v: any) => {
+  if (_.isNumber(v)) {
+    const numStr = v.toFixed(3);
+    if (_.endsWith(numStr, ".000")) {
+      return numStr.split(".")[0];
+    } else {
+      return parseFloat(numStr);
+    }
+  } else {
+    return v;
+  }
+}
 
 type Props = {
-  lineupSet: Record<string, any>,
+  players: Array<Record<string, any>>,
+  rosterStatsByCode: Record<string, any>,
+  teamSeasonLookup: string,
   showHelp: boolean,
   showDetailsOverride?: boolean
 };
-const TeamPlayTypeDiagView: React.FunctionComponent<Props> = ({lineupSet, showHelp, showDetailsOverride}) => {
+const TeamPlayTypeDiagView: React.FunctionComponent<Props> = ({players, rosterStatsByCode, teamSeasonLookup, showHelp, showDetailsOverride}) => {
+
+  // Repeat the logic in PlayerTypeTypeDiagView:
+
+  const perPlayCategoryAssistNetwork = _.chain(players).map((player) => {
+    const allPlayers = PlayTypeUtils.buildPlayerAssistCodeList(player);
+    const playerStyle = PlayTypeUtils.buildPlayerStyle(player);
+    const playerAssistNetwork = allPlayers.map((p) => {
+      const [ info, ignore ] = PlayTypeUtils.buildPlayerAssistNetwork(
+        p, player, playerStyle.totalScoringPlaysMade, playerStyle.totalAssists,
+        rosterStatsByCode
+      );
+      return info;
+    });
+    const posCategoryAssistNetwork = PlayTypeUtils.buildPosCategoryAssistNetwork(
+      playerAssistNetwork, rosterStatsByCode, undefined
+    );
+
+    /**/
+    console.log(`${player.code} ... vs ... ${JSON.stringify(posCategoryAssistNetwork, tidyNumbers, 3)}`);
+
+    return [ player.code || "??", posCategoryAssistNetwork ];
+  }).fromPairs().value();
+
+  //TODO: need unassisted as well
+
+  // This gets us to:
+  // [1] (player)[ { [pos]: <shot-type-stats> } ]   (pos=bh|wing|big)
+
+  // Then buildPosCategoryAssistNetwork goes:
+  // [2] player, (other_players)[ <shot-type-stats> ] => { [pos]: <shot-type-stats> }
+  // (and "player" is only used to inject examples in)
+
+  // So transform [1] to (pos)(players)[ <shot-type-stats> ] and then use [2] on each pos
+  // and that gives us a pos vs pos -> <shot-type-stats> as desired!
+
+  _.chain(PosFamilyNames).map((pos, ix) => {
+    const perPlayer = _.chain(perPlayCategoryAssistNetwork).mapValues((posAssistNetwork, playerCode) => {
+      const posStats = posAssistNetwork.filter(net => net?.order == ix)?.[0] || undefined;
+       return posStats ? [ {
+        ...posStats,
+        code: playerCode
+      } ] : [];
+    }).values().flatten().value();
+
+    const posPosCatAssistNetwork = PlayTypeUtils.buildPosCategoryAssistNetwork(
+      perPlayer, rosterStatsByCode, undefined
+    ); // pos vs <shot-type-stats> (order tells you which)
+
+/**/
+//console.log(`${pos} ... vs ... ${JSON.stringify(posPosCatAssistNetwork, tidyNumbers, 3)}`);
+
+    return [ pos, posPosCatAssistNetwork ];
+
+  }).fromPairs().value();
+
+
 
   return <span>
-      <span>
-        <b>Play Type Breakdown</b>
-      </span>
-      <br/>
-      <span>
-        <b>Scrambles off rebounds</b>
-      </span>
-      <br/>
-      <span>
-      {
-        JSON.stringify(_.chain(lineupSet).toPairs().filter((kv: [string, any]) => {
-          return kv[0].indexOf("scramble_") > 0
-        }).fromPairs().value())
-      }
-      </span>
-      <br/>
-      <span>
-        <b>Transition</b>
-      </span>
-      <br/>
-      <span>
-      {
-//TODO: these transition numbers seem way too high ... 400 vs 300 FGA and more importantly 60% FTR vs 16% (2019)
-//2015/6: FGAs are almost identical (273) but 50% FTR (138) vs 15% again, leading to noticeable difference
-// trans TO also slightly high (20% vs 13%)     
-        JSON.stringify(_.chain(lineupSet).toPairs().filter((kv: [string, any]) => {
-          return kv[0].indexOf("trans_") > 0
-        }).fromPairs().value())
-      }
-      </span>
+
     </span>;
 };
 export default TeamPlayTypeDiagView;
