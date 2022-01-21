fc0e976b98cbc35fa6fb2e67098b2554d2afda12
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2022-01-21 16:24:24 -0500

    Fixed some data duplication in 20/21 season, so upping the data last updated field

--- src/utils/internal-data/dataLastUpdated.ts
+++ src/utils/internal-data/dataLastUpdated.ts
@@ -9,7 +9,7 @@ export const dataLastUpdated: Record<string, number> = { //Update by executing .
   "Women_2018/9": 1554854428,
   "Men_2019/20": 1583968724,
   "Women_2019/20": 1583968724,
-  "Men_2020/21": 1617682994,
+  "Men_2020/21": 1617682995,
   "Women_2020/21": 1617682996,
   "Men_2021/22": parseInt(process.env.MEN_CURR_UPDATE || "0"),
   "Women_2021/22": parseInt(process.env.WOMEN_CURR_UPDATE || "0")
