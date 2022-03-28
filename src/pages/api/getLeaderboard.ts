07fe3e2f17e7ee1098ca180392292eaa0ce494e0
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2022-03-28 19:04:49 -0400

    Add cache control to GCS retrieval

--- src/pages/api/getLeaderboard.ts
+++ src/pages/api/getLeaderboard.ts
@@ -3,6 +3,7 @@ import { NextApiResponse, NextApiRequest } from 'next';
 import fetch from 'isomorphic-unfetch';
 
 import queryString from "query-string";
+import { processEnv } from 'next/dist/lib/load-env-config';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const url = require('url').parse(req.url);
@@ -23,10 +24,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return undefined;
     } else {
         try {
-            const resp = await fetch(`https://storage.googleapis.com/${process.env.LEADERBOARD_BUCKET}/${parsed.src}_${parsed.oppo}_${parsed.gender}_${parsed.year}_${parsed.tier}.json`, {
+            const cacheId = process.env.VERCEL_URL || "default_cache_control";
+            const resp = await fetch(`https://storage.googleapis.com/${process.env.LEADERBOARD_BUCKET}/${parsed.src}_${parsed.oppo}_${parsed.gender}_${parsed.year}_${parsed.tier}.json?cacheId=${cacheId}`, {
                 method: 'get'
             });
-            const respJson = await resp.json()
+            const respJson = await resp.json();
             res.setHeader("Cache-Control", "s-maxage=28800"); // requests that the CDN cache this for 12 hours or until the app redeploys
             res.status(200).json(respJson);
         } catch (e) {
