89cc05a9f88870e4c9e46c92a090b171d4f6e4fe
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2023-06-03 13:26:16 -0400

    Start reformatting code following addition of prettifier

--- src/utils/LeaderboardUtils.ts
+++ src/utils/LeaderboardUtils.ts
@@ -1,183 +1,272 @@
-
 // Lodash:
 import _ from "lodash";
 
 // Next imports:
-import fetch from 'isomorphic-unfetch';
+import fetch from "isomorphic-unfetch";
 import { ParamDefaults } from "./FilterModels";
 import { DateUtils } from "./DateUtils";
 
 /** Information about transfer (typically indexed by player code) */
 export type TransferModel = {
-   f: string, //(team transferring from)
-   t?: string //(team transferring to)
+  f: string; //(team transferring from)
+  t?: string; //(team transferring to)
 };
 
 export class LeaderboardUtils {
+  //////////////////////////////////////
 
-   //////////////////////////////////////
-   
-   // Top level methods
+  // Top level methods
 
-   /** Get multiple years and tier of player leaderboards, plus transfer info for a given year 
-    * Returns a list of JSONs, the last transfersYear.size of which are transfers
+  /** Get multiple years and tier of player leaderboards, plus transfer info for a given year
+   * Returns a list of JSONs, the last transfersYear.size of which are transfers
    */
-   static getMultiYearPlayerLboards(
-      dataSubEventKey: "all" | "t100" | "conf" | "all-lowvol",
-      gender: string, fullYear: string, tier: string,
-      transferYears: string[], otherYears: string[],
-   ): Promise<any[]> {
-      return LeaderboardUtils.getMultiYearLboards(
-         gender, fullYear, tier, transferYears, otherYears,
-         (gender: string, subYear: string, inTier: string) => 
-         dataSubEventKey == "all-lowvol" ? [ 
-            LeaderboardUtils.getPlayerUrl("all", gender, subYear, inTier),
-            LeaderboardUtils.getPlayerUrl("lowvol", gender, subYear, inTier) 
+  static getMultiYearPlayerLboards(
+    dataSubEventKey: "all" | "t100" | "conf" | "all-lowvol",
+    gender: string,
+    fullYear: string,
+    tier: string,
+    transferYears: string[],
+    otherYears: string[]
+  ): Promise<any[]> {
+    return LeaderboardUtils.getMultiYearLboards(
+      gender,
+      fullYear,
+      tier,
+      transferYears,
+      otherYears,
+      (gender: string, subYear: string, inTier: string) =>
+        dataSubEventKey == "all-lowvol"
+          ? [
+              LeaderboardUtils.getPlayerUrl("all", gender, subYear, inTier),
+              LeaderboardUtils.getPlayerUrl("lowvol", gender, subYear, inTier),
+            ]
+          : [
+              LeaderboardUtils.getPlayerUrl(
+                dataSubEventKey,
+                gender,
+                subYear,
+                inTier
+              ),
+            ]
+    );
+  }
 
-         ] : [ 
-            LeaderboardUtils.getPlayerUrl(dataSubEventKey, gender, subYear, inTier) 
-         ]
-      );
-   }   
-
-   /** Get multiple years and tier of player/lineup leaderboards, plus transfer info for a given year 
-    * Returns a list of JSONs, the last transfersYear.size of which are transfers
+  /** Get multiple years and tier of player/lineup leaderboards, plus transfer info for a given year
+   * Returns a list of JSONs, the last transfersYear.size of which are transfers
    */
-   private static getMultiYearLboards(
-      gender: string, fullYear: string, tier: string,
-      transferYears: string[], otherYears: string[],
-      getUrl: (gender: string, subYear: string, inTier: string) => string[]
-   ): Promise<any[]> {
-      const year = fullYear.substring(0, 4);
-   
-      const years = _.filter(DateUtils.lboardYearListWithExtra, inYear => (year == "All") || (inYear == fullYear) || _.some(otherYears, y => y == inYear));
-      const tiers = _.filter([ "High", "Medium", "Low" ], inTier => (tier == "All") || (inTier == tier));
-   
-      const yearsAndTiers = _.flatMap(years, inYear => tiers.map(inTier => [ inYear, inTier ]));
-   
-      const fetchAll = Promise.all(
-         _.flatMap(yearsAndTiers, ([ inYear, inTier ]) => {
-            const subYear = inYear.substring(0, 4);
-            return getUrl(gender, subYear, inTier).map(url => fetch(url)
-               .then((response: fetch.IsomorphicResponse) => {
-                  return response.ok ? 
-                     response.json().then((j: any) => { //(tag the tier in)
-                        if (tier == "All") j.tier = inTier;
-                        return j;
-                     }) 
-                     : 
-                     Promise.resolve({ error: "No data available" });
-               })
-            );
-         }).concat(
-            transferYears.map(transferYear => {
-               const transferJsonPath = _.thru(transferYear, __ => {
-                  if (!transferYear || (transferYear.substring(0, 4) == DateUtils.offseasonPredictionYear.substring(0, 4))) {
-                     return `/api/getTransfers?transferMode=${transferYear || ""}`;
-                  } else {
-                     return `/leaderboards/roster_movement/transfers_${transferYear.substring(0, 4)}.json`;
-                  }
-               });
-               return transferYear ? fetch(transferJsonPath).then((response: fetch.IsomorphicResponse) => {
-                  return response.ok ? response.json() : Promise.resolve({})
-               }) : Promise.resolve({});
-            })
-         )
-      );  
-      return fetchAll;  
-   }
+  private static getMultiYearLboards(
+    gender: string,
+    fullYear: string,
+    tier: string,
+    transferYears: string[],
+    otherYears: string[],
+    getUrl: (gender: string, subYear: string, inTier: string) => string[]
+  ): Promise<any[]> {
+    const year = fullYear.substring(0, 4);
 
-   /** Get a single year of player leaderboard for a single tier (mostly for older years when there was only one tier) */
-   static getSingleYearPlayerLboards(
-      dataSubEventKey: "all" | "t100" | "conf",
-      gender: string, fullYear: string, tier: string
-   ): Promise<any> {
-      return LeaderboardUtils.getSingleYearLboards(
-         gender, fullYear, tier, 
-         (gender: string, subYear: string, inTier: string) => LeaderboardUtils.getPlayerUrl(dataSubEventKey, gender, subYear, inTier)
-      );
-   }
+    const years = _.filter(
+      DateUtils.lboardYearListWithExtra,
+      (inYear) =>
+        year == "All" ||
+        inYear == fullYear ||
+        _.some(otherYears, (y) => y == inYear)
+    );
+    const tiers = _.filter(
+      ["High", "Medium", "Low"],
+      (inTier) => tier == "All" || inTier == tier
+    );
 
-   /** Get a single year of player/lineup leaderboard for a single tier (mostly for older years when there was only one tier) */
-   private static getSingleYearLboards(
-      gender: string, fullYear: string, tier: string,
-      getUrl: (gender: string, subYear: string, inTier: string) => string
-   ): Promise<any> {
-      const year = fullYear.substring(0, 4);
+    const yearsAndTiers = _.flatMap(years, (inYear) =>
+      tiers.map((inTier) => [inYear, inTier])
+    );
 
-      return fetch(getUrl(gender, year, tier))
-         .then((response: fetch.IsomorphicResponse) => {
-            return (response.ok ? response.json() : Promise.resolve({ error: "No data available" }))
-         });
-   }
-   
-   /** Get a single year (but multiple tiers) of team stats
-   */
-   static getMultiYearTeamStats(
-      gender: string, fullYear: string, tier: string, otherYears: string[],
-   ): Promise<any[]> {
-      const year = fullYear.substring(0, 4);
-   
-      const years = _.filter(DateUtils.lboardYearListWithExtra, inYear => (year == "All") || (inYear == fullYear) || _.some(otherYears, y => y == inYear));
-      const tiers = _.filter([ "High", "Medium", "Low" ], inTier => (tier == "All") || (inTier == tier));
-   
-      const yearsAndTiers = _.flatMap(years, inYear => tiers.map(inTier => [ inYear, inTier ]));
-   
-      const fetchAll = Promise.all(yearsAndTiers.map(([ inYear, inTier ]) => {
+    const fetchAll = Promise.all(
+      _.flatMap(yearsAndTiers, ([inYear, inTier]) => {
         const subYear = inYear.substring(0, 4);
-        return fetch(LeaderboardUtils.getTeamStatsUrl(gender, subYear, inTier))
-          .then((response: fetch.IsomorphicResponse) => {
-            return response.ok ? 
-            response.json().then((j: any) => { //(tag the tier in)
-              if (tier == "All") j.tier = inTier;
-              return j;
-            }) : 
-            Promise.resolve({ error: "No data available" });
+        return getUrl(gender, subYear, inTier).map((url) =>
+          fetch(url).then((response: fetch.IsomorphicResponse) => {
+            return response.ok
+              ? response.json().then((j: any) => {
+                  //(tag the tier in)
+                  if (tier == "All") j.tier = inTier;
+                  return j;
+                })
+              : Promise.resolve({ error: "No data available" });
+          })
+        );
+      }).concat(
+        transferYears.map((transferYear) => {
+          const transferJsonPath = _.thru(transferYear, (__) => {
+            if (
+              !transferYear ||
+              transferYear.substring(0, 4) ==
+                DateUtils.offseasonPredictionYear.substring(0, 4)
+            ) {
+              return `/api/getTransfers?transferMode=${transferYear || ""}`;
+            } else {
+              return `/leaderboards/roster_movement/transfers_${transferYear.substring(
+                0,
+                4
+              )}.json`;
+            }
           });
-      }));  
-      return fetchAll;  
-   }
+          return transferYear
+            ? fetch(transferJsonPath).then(
+                (response: fetch.IsomorphicResponse) => {
+                  return response.ok ? response.json() : Promise.resolve({});
+                }
+              )
+            : Promise.resolve({});
+        })
+      )
+    );
+    return fetchAll;
+  }
 
-   //////////////////////////////////////
-   
-   // Lower level utils
+  /** Get a single year of player leaderboard for a single tier (mostly for older years when there was only one tier) */
+  static getSingleYearPlayerLboards(
+    dataSubEventKey: "all" | "t100" | "conf",
+    gender: string,
+    fullYear: string,
+    tier: string
+  ): Promise<any> {
+    return LeaderboardUtils.getSingleYearLboards(
+      gender,
+      fullYear,
+      tier,
+      (gender: string, subYear: string, inTier: string) =>
+        LeaderboardUtils.getPlayerUrl(dataSubEventKey, gender, subYear, inTier)
+    );
+  }
 
-   /** Fetch the requested player leaderboard either from GCS or static storage */
-   static readonly getPlayerUrl = (oppo: string, gender: string, subYear: string, inTier: string) => {
-      if (DateUtils.inSeasonYear.startsWith(subYear)) { // Access from dynamic storage
-        return `/api/getLeaderboard?src=players&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
-      } else { //archived
-        return `/leaderboards/lineups/players_${oppo}_${gender}_${subYear}_${inTier}.json`;
+  /** Get a single year of player/lineup leaderboard for a single tier (mostly for older years when there was only one tier) */
+  private static getSingleYearLboards(
+    gender: string,
+    fullYear: string,
+    tier: string,
+    getUrl: (gender: string, subYear: string, inTier: string) => string
+  ): Promise<any> {
+    const year = fullYear.substring(0, 4);
+
+    return fetch(getUrl(gender, year, tier)).then(
+      (response: fetch.IsomorphicResponse) => {
+        return response.ok
+          ? response.json()
+          : Promise.resolve({ error: "No data available" });
       }
-   }
-    
-   /** Fetch the requested lineup leaderboard either from GCS or static storage */
-   static readonly getLineupUrl = (oppo: string, gender: string, subYear: string, inTier: string) => {
-      if (DateUtils.inSeasonYear.startsWith(subYear)) { // Access from dynamic storage
-        return `/api/getLeaderboard?src=lineups&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
-      } else { //archived
-        return `/leaderboards/lineups/lineups_${oppo}_${gender}_${subYear}_${inTier}.json`;
-      }
-   }
+    );
+  }
 
-   /** Fetch the requested team leaderboard either from GCS or static storage */
-   static readonly getTeamUrl = (oppo: string, gender: string, subYear: string, inTier: string) => {
-      if (DateUtils.inSeasonYear.startsWith(subYear)) { // Access from dynamic storage
-        return `/api/getLeaderboard?src=teams&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
-      } else { //archived
-        return `/leaderboards/lineups/teams_${oppo}_${gender}_${subYear}_${inTier}.json`;
-      }
-   }
+  /** Get a single year (but multiple tiers) of team stats
+   */
+  static getMultiYearTeamStats(
+    gender: string,
+    fullYear: string,
+    tier: string,
+    otherYears: string[]
+  ): Promise<any[]> {
+    const year = fullYear.substring(0, 4);
 
-   /** Fetch the requested team stats either from GCS or static storage */
-   static readonly getTeamStatsUrl = (gender: string, subYear: string, inTier: string) => {
-      return `/leaderboards/lineups/team_stats_all_${gender}_${subYear}_${inTier}.json`;
+    const years = _.filter(
+      DateUtils.lboardYearListWithExtra,
+      (inYear) =>
+        year == "All" ||
+        inYear == fullYear ||
+        _.some(otherYears, (y) => y == inYear)
+    );
+    const tiers = _.filter(
+      ["High", "Medium", "Low"],
+      (inTier) => tier == "All" || inTier == tier
+    );
 
-      //TODO: this doesn't currently exist:
-      // if (DateUtils.inSeasonYear.startsWith(subYear)) { // Access from dynamic storage
-      //   return `/api/getTeamStats?src=teams&gender=${gender}&year=${subYear}&tier=${inTier}`;
-      // } else { //archived
-      //   return `/leaderboards/lineups/team_stats_all_${gender}_${subYear}_${inTier}.json`;
-      // }
-   }
+    const yearsAndTiers = _.flatMap(years, (inYear) =>
+      tiers.map((inTier) => [inYear, inTier])
+    );
+
+    const fetchAll = Promise.all(
+      yearsAndTiers.map(([inYear, inTier]) => {
+        const subYear = inYear.substring(0, 4);
+        return fetch(
+          LeaderboardUtils.getTeamStatsUrl(gender, subYear, inTier)
+        ).then((response: fetch.IsomorphicResponse) => {
+          return response.ok
+            ? response.json().then((j: any) => {
+                //(tag the tier in)
+                if (tier == "All") j.tier = inTier;
+                return j;
+              })
+            : Promise.resolve({ error: "No data available" });
+        });
+      })
+    );
+    return fetchAll;
+  }
+
+  //////////////////////////////////////
+
+  // Lower level utils
+
+  /** Fetch the requested player leaderboard either from GCS or static storage */
+  static readonly getPlayerUrl = (
+    oppo: string,
+    gender: string,
+    subYear: string,
+    inTier: string
+  ) => {
+    if (DateUtils.inSeasonYear.startsWith(subYear)) {
+      // Access from dynamic storage
+      return `/api/getLeaderboard?src=players&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
+    } else {
+      //archived
+      return `/leaderboards/lineups/players_${oppo}_${gender}_${subYear}_${inTier}.json`;
+    }
+  };
+
+  /** Fetch the requested lineup leaderboard either from GCS or static storage */
+  static readonly getLineupUrl = (
+    oppo: string,
+    gender: string,
+    subYear: string,
+    inTier: string
+  ) => {
+    if (DateUtils.inSeasonYear.startsWith(subYear)) {
+      // Access from dynamic storage
+      return `/api/getLeaderboard?src=lineups&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
+    } else {
+      //archived
+      return `/leaderboards/lineups/lineups_${oppo}_${gender}_${subYear}_${inTier}.json`;
+    }
+  };
+
+  /** Fetch the requested team leaderboard either from GCS or static storage */
+  static readonly getTeamUrl = (
+    oppo: string,
+    gender: string,
+    subYear: string,
+    inTier: string
+  ) => {
+    if (DateUtils.inSeasonYear.startsWith(subYear)) {
+      // Access from dynamic storage
+      return `/api/getLeaderboard?src=teams&oppo=${oppo}&gender=${gender}&year=${subYear}&tier=${inTier}`;
+    } else {
+      //archived
+      return `/leaderboards/lineups/teams_${oppo}_${gender}_${subYear}_${inTier}.json`;
+    }
+  };
+
+  /** Fetch the requested team stats either from GCS or static storage */
+  static readonly getTeamStatsUrl = (
+    gender: string,
+    subYear: string,
+    inTier: string
+  ) => {
+    return `/leaderboards/lineups/team_stats_all_${gender}_${subYear}_${inTier}.json`;
+
+    //TODO: this doesn't currently exist:
+    // if (DateUtils.inSeasonYear.startsWith(subYear)) { // Access from dynamic storage
+    //   return `/api/getTeamStats?src=teams&gender=${gender}&year=${subYear}&tier=${inTier}`;
+    // } else { //archived
+    //   return `/leaderboards/lineups/team_stats_all_${gender}_${subYear}_${inTier}.json`;
+    // }
+  };
 }
