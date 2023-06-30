ce73077e1feb08faf0f3a0f9659ba9e5051102ac
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2023-06-30 16:18:21 -0400

    Move to GA4

--- src/utils/GoogleAnalytics.ts
+++ src/utils/GoogleAnalytics.ts
@@ -1,22 +1,21 @@
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  console.log('GA: init=' + (undefined != process.env.GA_KEY));
+  console.log("GA: init=" + (undefined != process.env.GA_KEY));
   ReactGA.initialize(process.env.GA_KEY as string, {
-    testMode: process.env.NODE_ENV === 'test',
+    testMode: process.env.NODE_ENV === "test",
   });
-}
+};
 export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-}
-export const logEvent = (category = '', action = '') => {
+};
+export const logEvent = (category = "", action = "") => {
   if (category && action) {
     ReactGA.event({ category, action });
   }
-}
-export const logException = (description = '', fatal = false) => {
+};
+export const logException = (description = "", fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal });
+    //(ReactGA.pageView no longer supported, since GA4)
   }
-}
+};
