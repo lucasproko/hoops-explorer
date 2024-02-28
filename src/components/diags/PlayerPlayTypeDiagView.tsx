8cd96f06830200e98eb1036d8b09c6582af3e0bd
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2024-02-28 12:40:45 -0500

    Fix missing arg

--- src/components/diags/PlayerPlayTypeDiagView.tsx
+++ src/components/diags/PlayerPlayTypeDiagView.tsx
@@ -79,6 +79,7 @@ const PlayerPlayTypeDiagView: React.FunctionComponent<Props> = ({
         showGrades="false"
         teamSeasonLookup={teamSeasonLookup}
         showHelp={showHelp}
+        quickSwitchOverride={undefined}
       />
     );
   }
