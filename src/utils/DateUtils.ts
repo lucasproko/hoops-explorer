f6991ef9c6b28b8c3d75dcdbea5b8c6575886aad
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2024-10-15 10:46:48 -0400

    Remove off-season mode for front page

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -15,7 +15,7 @@ export class DateUtils {
   static readonly frontPageIsOffseasonLeaderboard: Boolean = true;
 
   /** During portal mania show the off-season leaderboard with extra in/out metrics */
-  static readonly showOffseasonMetrics: Boolean = true;
+  static readonly showOffseasonMetrics: Boolean = false;
 
   /** This year is being written to GCS daily, others are statically part of the website - DO NOT USE OUTSIDE OF THIS CONTEXT */
   static readonly inSeasonYear: string = "2023/24";
