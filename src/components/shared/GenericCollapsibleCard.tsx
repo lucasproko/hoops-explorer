6dec143492dc7cd7f3d3e30ac5beed136b0c1037
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2024-11-29 12:33:25 -0500

    Fix old bug causing cards to jump around when hidden

--- src/components/shared/GenericCollapsibleCard.tsx
+++ src/components/shared/GenericCollapsibleCard.tsx
@@ -1,64 +1,92 @@
 // React imports:
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Next imports:
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 // Bootstrap imports:
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import Collapse from 'react-bootstrap/Collapse';
-import Container from 'react-bootstrap/Container';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Collapse from "react-bootstrap/Collapse";
+import Container from "react-bootstrap/Container";
 
 type Props = {
   /** Much smaller margin => looks good for table-oriented displays */
-  readonly minimizeMargin: boolean,
-  readonly title: string,
-  readonly summary?: string,
-  readonly helpLink?: string,
-  readonly startClosed?: boolean,
-  readonly onShowHide?: (nowShown: boolean) => void
-}
+  readonly minimizeMargin: boolean;
+  readonly title: string;
+  readonly summary?: string;
+  readonly helpLink?: string;
+  readonly startClosed?: boolean;
+  readonly onShowHide?: (nowShown: boolean) => void;
+};
 
-const GenericCollapsibleCard: React.FunctionComponent<Props> = ({children, minimizeMargin, title, summary, helpLink, startClosed, onShowHide}) => {
-  const [ showTable, toggleShowTable ] = useState((undefined == startClosed) || !startClosed);
+const GenericCollapsibleCard: React.FunctionComponent<Props> = ({
+  children,
+  minimizeMargin,
+  title,
+  summary,
+  helpLink,
+  startClosed,
+  onShowHide,
+}) => {
+  const [showTable, toggleShowTable] = useState(
+    undefined == startClosed || !startClosed
+  );
 
   const showSummaryIfHidden = () => {
     if (!showTable && summary) {
       return <div className="">{summary}</div>;
     }
-  }
+  };
   const optionalHelpLink = () => {
-    if (helpLink ) {
-      return <span className="float-right"><a target="_blank" href={helpLink}><small>(?)</small></a></span>;
+    if (helpLink) {
+      return (
+        <span className="float-right">
+          <a target="_blank" href={helpLink}>
+            <small>(?)</small>
+          </a>
+        </span>
+      );
     }
-  }
+  };
 
-  const cardBodyStyle = minimizeMargin ? {paddingLeft: "5px", paddingRight: "5px"} : {};
-  const containerStyle = minimizeMargin ? {paddingLeft: 0, paddingRight: 0} : {};
-  const titleStyle = minimizeMargin ? {paddingLeft: "15px", paddingRight: "15px"} : {};
+  const cardBodyStyle = minimizeMargin
+    ? { paddingLeft: "5px", paddingRight: "5px" }
+    : {};
+  const containerStyle = minimizeMargin
+    ? { paddingLeft: 0, paddingRight: 0 }
+    : {};
+  const titleStyle = minimizeMargin
+    ? { paddingLeft: "15px", paddingRight: "15px" }
+    : {};
 
-  return <Card className="w-100">
-    <Card.Body style={cardBodyStyle}>
-      <Card.Title style={titleStyle}>
-        <span><a href="#" onClick={() => { 
-          const newSetting = !showTable;
-          toggleShowTable(newSetting); 
-          if (onShowHide) onShowHide(newSetting);
-          return false;
-        } }>
-          ({showTable ? "+" : "-"}) {title}
-        </a></span>
-        {optionalHelpLink()}
-      </Card.Title>
-      {showSummaryIfHidden()}
-      <Collapse in={showTable}>
-        <Container style={containerStyle}>
-          {children}
-        </Container>
-      </Collapse>
-    </Card.Body>
-  </Card>;
-}
+  return (
+    <Card className="w-100">
+      <Card.Body style={cardBodyStyle}>
+        <Card.Title style={titleStyle}>
+          <span>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                const newSetting = !showTable;
+                toggleShowTable(newSetting);
+                if (onShowHide) onShowHide(newSetting);
+                return false;
+              }}
+            >
+              ({showTable ? "+" : "-"}) {title}
+            </a>
+          </span>
+          {optionalHelpLink()}
+        </Card.Title>
+        {showSummaryIfHidden()}
+        <Collapse in={showTable}>
+          <Container style={containerStyle}>{children}</Container>
+        </Collapse>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default GenericCollapsibleCard;
