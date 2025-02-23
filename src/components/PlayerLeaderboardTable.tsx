7d125840d74cd5a376d9815c9079bcf978f82001
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2025-02-22 19:15:04 -0500

    Fixed player CSV export filename

--- src/components/PlayerLeaderboardTable.tsx
+++ src/components/PlayerLeaderboardTable.tsx
@@ -2248,7 +2248,7 @@ const PlayerLeaderboardTable: React.FunctionComponent<Props> = ({
                     const url = URL.createObjectURL(blob);
                     const link = document.createElement("a");
                     link.href = url;
-                    link.download = "all_team_explorer_stats.csv";
+                    link.download = "all_player_stats.csv";
                     document.body.appendChild(link);
                     link.click();
                     document.body.removeChild(link);
@@ -2288,7 +2288,7 @@ const PlayerLeaderboardTable: React.FunctionComponent<Props> = ({
                     const url = URL.createObjectURL(blob);
                     const link = document.createElement("a");
                     link.href = url;
-                    link.download = "filtered_team_explorer_stats.csv";
+                    link.download = "filtered_player_stats.csv";
                     document.body.appendChild(link);
                     link.click();
                     document.body.removeChild(link);
